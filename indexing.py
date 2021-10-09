680a59a0cee1df1285d9ccf033f4460b74b1ec23
vishakha287 <91821631+vishakha287@users.noreply.github.com>
2021-10-09 18:00:05 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,9 @@
+'''
+2 types of index
++ve index- starts from left most item
+-ve index- starts from right most item
+'''
+name ='sigma'
+print(name[0]) #first char
+print(name[-1]) #last
+print(name[-2]) #sec last
